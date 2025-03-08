608e35878662591e2f409516e6d9b6c477fcabab
YawoSadji <yawo.sadji@gmx.com>
2025-03-07 19:39:16 -0500

    final commit

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -3,6 +3,12 @@ import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
 
@@ -73,7 +79,11 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   const fetcher = useFetcher();
-  const favorite = contact.favorite;
+
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
+
   return (
     <fetcher.Form method="post">
       <button
